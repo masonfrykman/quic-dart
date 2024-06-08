db3416cd4c4a33e961cd5fe639dd6b211498b1f0
masonfrykman <masonfrykman@proton.me>
2024-06-08 11:51:52 -0500

    random reset token generator

--- lib/src/connection_id.dart
+++ lib/src/connection_id.dart
@@ -1,9 +1,21 @@
+import 'dart:math';
+import 'dart:typed_data';
+
 class QUICConnectionId {
   QUICConnectionIdType type;
   int sequence;
-  List<int> statelessResetToken;
+  Uint8List statelessResetToken = Uint8List(16);
+
+  bool get statelessResetTokenIsCorrectLength =>
+      statelessResetToken.lengthInBytes == 16;
 
   QUICConnectionId(this.type, this.sequence, this.statelessResetToken);
+
+  QUICConnectionId.randomResetToken(this.type, this.sequence) {
+    for (int i = 0; i < 16; i++) {
+      statelessResetToken[i] = Random().nextInt(256);
+    }
+  }
 }
 
 enum QUICConnectionIdType { source, destination }
